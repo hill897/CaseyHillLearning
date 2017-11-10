72f49d6612965fc09eade7ef908ca5f4aeee8f64
Casey Hill <casey.hill7@gmail.com>
2017-11-09 19:50:04 -0600

    more javascript exercises

--- public/media/greeting.js
+++ public/media/greeting.js
@@ -1,8 +1,11 @@
-var name = prompt("what is your name?");
+var firstName = prompt("what is your first name?") || "Visitor";
+
+var lastName = prompt("what is your last name?") || "McPoopy Pants";
+
 var output = document.querySelector("#greeting");
 
-if (name) {
-  output.innerHTML = "<p>" + name + " is logged in .<p>";
+if (firstName && lastName) {
+  output.innerHTML = "<p>" + firstName + " " + lastName + " is logged in .<p>";
 }
 else {
   output.innerHTML = "<p> Invalid Username </p>";
